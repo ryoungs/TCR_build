2686f9cd2311183b92c254b267d6b9214a064d02
Ralph Youngs <r.youngs@verizon.net>
2024-11-26 11:19:25 -0500

    remove archive dir

--- tcr_pkg/backup_clean.py
+++ tcr_pkg/backup_clean.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import datetime as dt
 import tcr_pkg.delete_files_in_directory as df
+# import delete_files_in_directory as df  # for testing
 
 # backup database adding yesterday's date to backup file name
 # TBD throws an error if dbase is not present!
@@ -9,13 +10,16 @@ def backup_clean():
     d0 = dt.date.today() - dt.timedelta(days=1)  # Yesterday's date
     dY = d0.isoformat()
 
+    # Archive the database to TCR_archive folder
     src = './acc_animals.db'
-    dest = './archive/acc_animals_' + dY + '.db'
-    path = '../TCR/'
+    dest = './../TCR_archives/acc_animals_' + dY + '.db'
 
     if os.path.isfile(src):
         shutil.copy2(src, dest)
         os.remove(src)
+    
+    # Set path to remove images, thumbs, home, and foster pages 
+    path = '../TCR/'
     
     if os.path.isfile(path+'index.html'):
         os.remove(path+'index.html')
